7604f8d512bf4ce3de0e2e3a77e8351737144910
alejandro <alejozu9@gmail.com>
2014-12-03 09:56:57 -0600

    Factorial

--- application.py
+++ application.py
@@ -1,3 +1,33 @@
-# Aqui escribe tu codigo
+#-*- coding: utf-8-*-
 
-print "Hello World"
+print "	•••••••••••••••••••••••••••••"
+print " 	•    •Número Factorial•     •"
+print "	•••••••••••••••••••••••••••••"
+
+def factorial(numero):  
+	if numero == 0 or numero == 1:
+		residuo = 1
+	elif numero > 1:
+		residuo = numero*factorial(numero-1)
+	return residuo
+
+num = True
+while num == True:
+	try:
+		digitos=int(raw_input("Ingrese Un Número: "))
+		print "Su Factorial es: %s"%factorial(digitos)
+		re = True
+		while re == True:
+			reingreso = raw_input("Desea volver a ingresar otro valor Si/No: ")
+			try:
+				if reingreso.isalpha()==True:
+					if reingreso.lower()=="si":
+						re=False
+					elif reingreso.lower()=="no":
+						num = False
+						print "Adios"
+						break
+			except:
+				num = True
+	except ValueError,NameError:
+		print "No se aceptan datos Alfabeticos"
